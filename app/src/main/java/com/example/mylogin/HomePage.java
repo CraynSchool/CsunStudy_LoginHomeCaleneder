d38e9020429fc46890aedd087eb2beaafdca1f26
Crayn <crayn.sanchez.college@gmail.com>
2023-10-11 10:10:06 -0700

    calender page added

--- app/src/main/java/com/example/mylogin/HomePage.java
+++ app/src/main/java/com/example/mylogin/HomePage.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*For calender button*/
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+
 public class HomePage extends AppCompatActivity {
+    public Button cal_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        cal_btn = (Button) findViewById(R.id.calender_btn); //link btn in home xml
+
+        cal_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, CalenderPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
